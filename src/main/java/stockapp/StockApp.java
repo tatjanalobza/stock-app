647d64ba36ab12632d827588013a5bf9c3b90538
Istvan Szabo <sz.pisti1@gmail.com>
2021-02-05 13:59:03 +0100

    Application setup

---
+++ src/main/java/stockapp/StockApp.java
@@ -0,0 +1,12 @@
+package stockapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockApp {
+    public static void main(String[] args) {
+        SpringApplication.run(StockApp.class, args);
+    }
+
+}
